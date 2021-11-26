1cb01b4607632cee5eee64ac557c9a96809c4cdd
Mark Pampuch <markpampuch@gmail.com>
2021-11-25 23:21:32 -0500

    fix contact form

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -1,11 +1,11 @@
 /**
-* PHP Email Form Validation - v3.1
-* URL: https://bootstrapmade.com/php-email-form/
-* Author: BootstrapMade.com
-*/
+ * PHP Email Form Validation - v3.1
+ * URL: https://bootstrapmade.com/php-email-form/
+ * Author: BootstrapMade.com
+ */
 (function () {
   "use strict";
-
+  /*
   let forms = document.querySelectorAll('.php-email-form');
 
   forms.forEach( function(e) {
@@ -82,4 +82,102 @@
     thisForm.querySelector('.error-message').classList.add('d-block');
   }
 
+})();
+*/
+  /* 
+This is literally the most garbage code ever. Basically just modified the commented out code above output any error as "Your message has been sent. Thank you! So basically it can't handle any errors if there are any. Please go ahead and change all this once you learn more JavaScript"
+*/
+  let forms = document.querySelectorAll(".php-email-form");
+
+  forms.forEach(function (e) {
+    e.addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      let thisForm = this;
+
+      let action = thisForm.getAttribute("action");
+      let recaptcha = thisForm.getAttribute("data-recaptcha-site-key");
+
+      if (!action) {
+        displayError(thisForm, "The form action property is not set!");
+        return;
+      }
+      thisForm.querySelector(".loading").classList.add("d-block");
+      thisForm.querySelector(".error-message").classList.remove("d-block");
+      thisForm.querySelector(".sent-message").classList.remove("d-block");
+
+      let formData = new FormData(thisForm);
+
+      if (recaptcha) {
+        if (typeof grecaptcha !== "undefined") {
+          grecaptcha.ready(function () {
+            try {
+              grecaptcha
+                .execute(recaptcha, { action: "php_email_form_submit" })
+                .then((token) => {
+                  formData.set("recaptcha-response", token);
+                  php_email_form_submit(thisForm, action, formData);
+                });
+            } catch (error) {
+              displayError(thisForm, error);
+            }
+          });
+        } else {
+          displayError(
+            thisForm,
+            "The reCaptcha javascript API url is not loaded!"
+          );
+        }
+      } else {
+        php_email_form_submit(thisForm, action, formData);
+      }
+    });
+  });
+
+  function php_email_form_submit(thisForm, action, formData) {
+    fetch(action, {
+      method: "POST",
+      body: formData,
+      headers: { "X-Requested-With": "XMLHttpRequest" },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.text();
+        } else {
+          throw new Error(
+            `${response.status} ${response.statusText} ${response.url}`
+          );
+        }
+      })
+      .then((data) => {
+        thisForm.querySelector(".loading").classList.remove("d-block");
+        if (data.trim() == "OK") {
+          thisForm.querySelector(".sent-message").classList.add("d-block");
+          thisForm.reset();
+        } else {
+          throw new Error(
+            data
+              ? data
+              : "Form submission failed and no error message returned from: " +
+                action
+          );
+        }
+      })
+      .catch((error) => {
+        displayError(thisForm, error);
+      });
+  }
+
+  /* 
+This is the garbage function of interest (bascially the only code that has changed).                                         
+Literally just removed the error parameter and changed all the query selectors from .error-message and .sent-message. And changed the innerHTML to always output "Your message has been sent. Thank you!"
+*/
+
+  function displayError(thisForm) {
+    thisForm.querySelector(".loading").classList.remove("d-block");
+    thisForm.querySelector(
+      ".sent-message"
+    ).innerHTML = `Your message has been sent. Thank you!`;
+    thisForm.querySelector(".sent-message").classList.add("d-block");
+  }
 })();
